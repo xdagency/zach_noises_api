f0a13237bc647b8732aa1cdae7d69101c5fdc731
xdagency <pandabear00001@outlook.com>
2019-05-25 10:39:27 -0400

    Added GET route for single noise ID

--- routes/noiseRoutes.js
+++ routes/noiseRoutes.js
@@ -24,14 +24,44 @@ router.get('/noises', (req, res) => {
             // log errors
             console.log('Fetch all noises error:', error);
             
-            // send a internal server error
+            // send an internal server error
             res.status(500);
 
         })
 
 });
 
-router.get('noise/:id', (req, res) => {
+router.get('/noise/:id', (req, res) => {
+
+    // Get the noise ID from the request param
+    let noise_id = req.params.id;
+
+    // Search DB for the noise with that ID
+    Noises.where({ id: noise_id }).fetch()
+    
+    .then(result => {
+        
+        // If result is null, meaning nothing was found under that id then send a 404
+        if (result === null) {
+            res.status(404).send('No noise found.');
+            return;
+        }
+
+        // OK status & Send the results
+        res.status(200).json(result);
+        
+    })
+
+    .catch(error => {
+
+        // log errors
+        console.log('Fetch a single noise error:', error);
+
+        // send an internal server error
+        res.status(500);
+
+    })
+
 });
 
 
