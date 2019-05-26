3024b5adbfb3d08c56b270d35cac402291b74b0b
xdagency <pandabear00001@outlook.com>
2019-05-26 18:58:46 -0400

    Updated migrations path on prod

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
       client: 'pg',
       connection: process.env.DATABASE_URL,
       migrations: {
-        directory: __dirname + 'migrations'
+        directory: './migrations'
       }
     }
   
